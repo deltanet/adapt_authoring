58815fd921063cc9bd8537df190e2b50826543cf
Thomas Taylor <thomas.taylor@kineo.com>
2016-05-07 16:31:59 +0100

    Add form data restoration

--- frontend/src/core/editor/theming/views/editorThemingView.js
+++ frontend/src/core/editor/theming/views/editorThemingView.js
@@ -20,7 +20,8 @@ define(function(require){
     events: {
       'change .theme select': 'onThemeChanged',
       'change .preset select': 'onPresetChanged',
-      'click a.edit': 'showPresetEdit'
+      'click button.edit': 'showPresetEdit',
+      'click button.reset': 'restoreDefaultSettings'
     },
 
     initialize: function() {
@@ -39,8 +40,10 @@ define(function(require){
     },
 
     render: function() {
-      // set the selected theme flag
+      // set the selected theme/preset
       this.themes.findWhere({ name: this.model.get('_theme') }).set('_isSelected', true);
+      var preset = this.presets.findWhere({ _id: this.model.get('_themepreset') });
+      preset && preset.set('_isSelected', true);
 
       EditorOriginView.prototype.render.apply(this, arguments);
       this.renderForm();
@@ -57,14 +60,18 @@ define(function(require){
           model: selectedTheme,
           schemaType: selectedTheme.get('theme')
         });
+
+        var toRestore = Origin.editor.data.course.get('themeSettings') || this.getDefaultThemeSettings();
+        this.restoreFormSettings(toRestore);
+
         this.$('.form-container').html(this.form.el);
         this.$('.theme-customiser').show();
-        this.$('a.edit').show();
+        this.$('button.edit').show();
         Origin.trigger('theming:showPresetButton', true);
       }
       else {
         this.$('.theme-customiser').hide();
-        this.$('a.edit').hide();
+        this.$('button.edit').hide();
         Origin.trigger('theming:showPresetButton', false);
       }
     },
@@ -118,26 +125,45 @@ define(function(require){
     },
 
     updatePresetSelect: function() {
-      var presets = this.presets.where({ parentTheme: $('.theme select').val() });
+      console.log('updatePresetSelect');
+      var theme = $('.theme select').val();
+      var presets = this.presets.where({ parentTheme: theme });
       var select = this.$('.preset select');
       // remove options first
       $('option', select).remove();
       // add 'no presets'
-      select.append($('<option>', { value: "", disabled: 'disabled', selected: 'selected' }).text(window.polyglot.t('app.nopresets')));
+      select.append($('<option>', { value: "", selected: 'selected' }).text(window.polyglot.t('app.nopresets')));
       // add options
       _.each(presets, function(item, index) {
         select.append($('<option>', { value: item.get('_id') }).text(item.get('displayName')));
       }, this);
       // disable delect, hide manage preset buttons if empty
       if(presets.length > 0) {
+        // TODO check selected preset exists in db (in case deleted)
+        var selectedPreset = this.getSelectedPreset();
+        if(selectedPreset && selectedPreset.get('parentTheme') === theme) {
+          select.val(selectedPreset.get('_id'));
+        }
         select.attr('disabled', false);
-        this.$('a.edit').show();
+        this.$('button.edit').show();
+        this.$('button.reset').show();
       } else {
         select.attr('disabled', true);
-        this.$('a.edit').hide();
+        this.$('button.edit').hide();
+        this.$('button.reset').hide();
       }
+    },
 
-      // TODO check selected preset exists (show error message), and select it
+    restoreFormSettings: function(toRestore) {
+      if(!this.form || !this.form.el) return console.log('No form to restore...');
+
+      for(var key in toRestore) {
+        var el = $('[name=' + key + ']', this.form.el);
+        el.val(toRestore[key]);
+        if(el.hasClass('scaffold-color-picker')) {
+          el.css('background-color', toRestore[key]);
+        }
+      }
     },
 
     // checks form for errors, returns true if valid, false otherwise
@@ -236,6 +262,17 @@ define(function(require){
       return this.presets.findWhere({ '_isSelected': true });
     },
 
+    getDefaultThemeSettings: function() {
+      var defaults = {};
+      var props = this.getSelectedTheme().get('properties');
+      for (var key in props) {
+        if (props.hasOwnProperty(key)) {
+          defaults[key] = props[key].default;
+        }
+      }
+      return defaults;
+    },
+
     /**
     * Event handling
     */
@@ -247,6 +284,23 @@ define(function(require){
         model: new Backbone.Model({ presets: new Backbone.Collection(this.presets.where({ parentTheme: parentTheme })) })
       });
       $('body').append(pev.el);
+    },
+
+    restoreDefaultSettings: function(event) {
+      event && event.preventDefault();
+      var self = this;
+      Origin.Notify.confirm({
+        type: 'warning',
+        text: window.polyglot.t('app.restoredefaultstext'),
+        callback: function(confirmed) {
+          if(confirmed) {
+            var preset = self.getSelectedPreset();
+            console.log(preset);
+            var settings = (preset) ? preset.get('properties') : self.getDefaultThemeSettings();
+            self.restoreFormSettings(settings);
+          }
+        }
+      });
     },
 
     onEditPreset: function(data) {
@@ -285,15 +339,14 @@ define(function(require){
     },
 
     onPresetChanged: function(event) {
+      // unset old
+      this.presets.findWhere({ _isSelected: true }).set("_isSelected", false);
       // set _isSelected
       var presetId = $(event.currentTarget).val();
-      var preset = this.presets.findWhere({ _id: presetId }).set("_isSelected", true);
-      // set the form input values
-      var presetProps = preset.get('properties');
-      for(var key in presetProps) {
-        var el = $('[name=' + key + ']', this.form.el);
-        el.val(presetProps[key]);
-        if(el.hasClass('scaffold-color-picker')) el.css('background-color', presetProps[key]);
+      var preset = this.presets.findWhere({ _id: presetId });
+      if(preset) {
+        preset.set("_isSelected", true);
+        this.restoreFormSettings(preset.attributes);
       }
     },
 

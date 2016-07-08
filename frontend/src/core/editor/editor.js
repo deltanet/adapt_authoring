30e0d5abfe3796e250cb3373f58d433e8f4e35ec
Tom Taylor <tom@taylortom.co.uk>
2016-07-08 10:04:43 +0100

    Ui refresh (#1248)
    * Fix broken filter button
    
    * Change dashboard search/filter text to match Asset Management
    
    * Fix spacing
    
    * Add instant search to dashboard and asset management
    
    * Fix spacing
    
    * Add missing separator divs to sidebars
    
    * Change plugin management disabled colour to match colour scheme better
    
    * Add less var
    
    * Set sidebar filter padding
    
    * Space loading spinner correctly, remove nav bar loading spinner
    
    Fixes #1234
    
    * #1168 Updated header bar to work with 2560px width screens.
    
    * #626 and added white on the expanded state of the section icon.
    
    * #1225
    
    * Fix login image sizing
    
    * #1242
    
    * Fix padding issue
    
    * Tweak loading animation position
    
    * Added width and padding to to Add page/sub menu buttons to make them flush with the items below
    
    * Tweak page editor component padding
    
    * Shorten loading animation
    
    * Fix sidebar padding
    
    Fixes #1235
    
    * Tidy up user profile page
    
    * Fix issue with empty fieldsets being rendered
    
    Fixes #1244
    
    * Add fallback hard-coded dashboard link to sidebar back button
    
    Fixes #1238
    The whole Origin.dashboardRoute code needs to be rethought...

--- frontend/src/core/editor/editor.js
+++ frontend/src/core/editor/editor.js
@@ -444,7 +444,7 @@ define(function(require) {
           // update sidebar view
           Origin.sidebar.addView(new EditorMenuSidebarView().$el, {
             "backButtonText": "Back to courses",
-            "backButtonRoute": Origin.dashboardRoute
+            "backButtonRoute": Origin.dashboardRoute || '/#/dashboard'
           });
         }
         break;

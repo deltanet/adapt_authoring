94bff963468a436502f0801c23e93b721510af58
Tom Taylor <tom@taylortom.co.uk>
2016-07-12 23:40:18 +0100

    Ui refresh (#1261)
    * Remove custom min-width from sweetalert.css
    
    * Reduce sweetalert button font-size
    
    * Reposition sweetalert loading icons
    
    * Fix page editor drop-zone styling
    
    Fixes #1257
    
    * Fix login validation-text styling
    
    Fixes #1208
    
    * Remove blue flash on global menu close
    
    * Add bottom border to menu editor 'add' button section
    
    * Style context menu
    
    * Fix problem with context menu not being cleaned up on route
    
    * Replace all Backbone.history.navigate with Origin.router.navigate
    
    Using Backbone.history meant that routing was missed by the router (so
    handleRoute not called). Besides, Backbone.Router.navigate is a wrapper
    for the Backbone.history.navigate anyway.
    
    Partially fixes #1260
    
    * Make tag background colour on-brand
    
    * Tweak context menu style
    
    * Tweak dashboard project-tile style
    
    * Tweak tags popup style
    
    * Update asset management style
    
    * Tweak menu picker style
    
    * Tweak theme picker style

--- frontend/src/core/sidebar/views/sidebarItemView.js
+++ frontend/src/core/sidebar/views/sidebarItemView.js
@@ -125,7 +125,7 @@ define(function(require) {
     },
 
     closeProject: function() {
-      Backbone.history.navigate('#/dashboard');
+      Origin.router.navigate('#/dashboard');
     }
 
   });

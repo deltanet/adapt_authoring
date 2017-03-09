8197d97329319d632981cc75423773d0d8775ed4
Dan Gray <dan@sinensis.co.uk>
2017-03-09 14:23:12 +0000

    fixes #108

--- plugins/output/adapt/index.js
+++ plugins/output/adapt/index.js
@@ -651,6 +651,8 @@ function importCourseassets(metadata, courseassetsImported) {
     async.each(metadata.courseassets, function(courseasset, createdCourseasset) {
       courseasset._courseId = metadata.idMap[courseasset._courseId];
       courseasset._assetId = metadata.idMap[courseasset._assetId];
+      courseasset._contentTypeParentId = metadata.idMap[courseasset._contentTypeParentId];
+      courseasset.createdBy = app.usermanager.getCurrentUser();
       if (courseasset._assetId !== undefined) {
         plugin.create(courseasset, createdCourseasset);
       } else {

baec1df0677088d17a67c406199807d66c298e58
Dan Gray <dan@sinensis.co.uk>
2017-03-09 15:50:36 +0000

    Check for idMap match

--- plugins/output/adapt/index.js
+++ plugins/output/adapt/index.js
@@ -651,7 +651,9 @@ function importCourseassets(metadata, courseassetsImported) {
     async.each(metadata.courseassets, function(courseasset, createdCourseasset) {
       courseasset._courseId = metadata.idMap[courseasset._courseId];
       courseasset._assetId = metadata.idMap[courseasset._assetId];
-      courseasset._contentTypeParentId = metadata.idMap[courseasset._contentTypeParentId];
+      if (metadata.idMap[courseasset._contentTypeParentId]) {
+        courseasset._contentTypeParentId = metadata.idMap[courseasset._contentTypeParentId];
+      }
       courseasset.createdBy = app.usermanager.getCurrentUser();
       if (courseasset._assetId !== undefined) {
         plugin.create(courseasset, createdCourseasset);

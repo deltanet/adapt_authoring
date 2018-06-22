febfca24bc45f32b89a5dce1b295fad9440e56fc
Dan Gray <dan@sinensis.co.uk>
2018-06-22 14:56:43 +0100

    Fix issue #268 bulk asset upload errors

--- frontend/src/modules/assetManagement/views/assetManagementUploadAssetsView.js
+++ frontend/src/modules/assetManagement/views/assetManagementUploadAssetsView.js
@@ -49,7 +49,7 @@ define(function(require){
         if (!$.trim($(el).val())) {
           validated = false;
           $(el).addClass('input-error');
-          $(errormsg).text(window.polyglot.t('app.pleaseentervalue'));
+          $(errormsg).text(Origin.l10n.t('app.pleaseentervalue'));
         } else {
           $(el).removeClass('input-error');
           $(errormsg).text('');
@@ -59,7 +59,7 @@ define(function(require){
       if (this.model.isNew() && !uploadFile.val()) {
         validated = false;
         $(uploadFile).addClass('input-error');
-        $(uploadFileErrormsg).text(window.polyglot.t('app.pleaseaddfile'));
+        $(uploadFileErrormsg).text(Origin.l10n.t('app.pleaseaddfile'));
       } else {
         $(uploadFile).removeClass('input-error');
         $(uploadFileErrormsg).text('');

d33641b35487633f1c6c5ce82ed5fe733329a207
Dan Gray <dan@sinensis.co.uk>
2018-06-26 15:48:59 +0100

    Populate TenantDisplayName on install fixes #221

--- install.js
+++ install.js
@@ -402,7 +402,7 @@ function configureMasterTenant(callback) {
         addConfig({
           masterTenant: {
             name: result.masterTenantName,
-            displayName: result.masterTenantName
+            displayName: result.masterTenantDisplayName
           }
         });
         // check if the tenant name already exists
